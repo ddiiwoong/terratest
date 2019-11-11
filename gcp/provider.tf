4187e7d5f151eec0259a69616d1d7703578bc2bb
ddiiwoong <ddiiwoong@gmail.com>
2019-11-11 17:35:48 +0900

    ibm add

--- gcp/provider.tf
+++ gcp/provider.tf
@@ -1,6 +1,6 @@
 provider "google" {
   version     = "~> 2.5"
-  credentials = "${GCLOUD_SERVICE_KEY}"
+  credentials = "${var.GCLOUD_SERVICE_KEY}"
   project     = "sktcl2019"
   region      = "asia-northeast1"
 }
